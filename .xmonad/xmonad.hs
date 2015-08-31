4c7c136c376362ae1b43c9cb3ad21d37e240fe23
James LaChance <fatboyxpc@gmail.com>
2015-08-31 16:36:05 -0400

    Adding xmobar and xmonad.hs files

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,141 @@
+import Data.List
+import Graphics.X11.ExtraTypes.XF86
+import System.IO
+import XMonad hiding ( (|||) ) -- don't use the normal ||| operator
+import XMonad.Actions.PhysicalScreens
+import XMonad.Actions.UpdatePointer
+import XMonad.Config.Desktop
+import XMonad.Layout.LayoutCombinators
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.SetWMName
+import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.Run(spawnPipe)
+import qualified XMonad.StackSet as W
+
+-- http://xmonad.org/xmonad-docs/xmonad-contrib/XMonad-Layout-NoBorders.html
+import XMonad.Layout.NoBorders
+
+-- Sensible key names, rather than "modNmask"
+altMask = mod1Mask
+winMask = mod4Mask
+
+-- Rebind Mod to the Windows key
+myModMask = altMask
+
+myTerminal = "gnome-terminal"
+tall = Tall 1 (3/100) (1/2)
+myLayout = avoidStruts $ smartBorders $ Full ||| Mirror tall ||| tall
+
+myBorderWidth = 2
+
+windowPlacement = composeAll [
+            -- Skype Conversations
+            role =? "conversation" --> doShift "3",
+
+            -- Pidgin Conversations
+            role =? "ConversationsWindow" --> doShift "3",
+
+            -- Steam Login
+            className =? "Steam" <&&> fmap (isInfixOf "Steam Login") title --> doShift "1",
+
+            -- Steam Chat
+            className =? "Steam" <&&> fmap (isInfixOf "Chat") title --> doShift "3",
+
+            -- All Steam Windows (except previously mentioned)
+            className =? "Steam" --> doShift "9",
+
+            className =? "Google-chrome-stable" --> doShift "1",
+            className =? "Sublime_text" --> doShift "2",
+            className =? "VirtualBox" --> doShift "8",
+
+            -- Skype Windows (except previously mentioned)
+            className =? "Skype" --> doShift "9",
+
+            -- Pidgin Buddy List
+            role =? "buddy_list" --> doShift "9",
+
+            -- Fix for GIMP windows
+            className =? "Gimp" --> doFloat,
+
+            -- Fix for KeePass2
+            className =? "KeePass2" --> doFloat
+        ] where role = stringProperty "WM_WINDOW_ROLE"
+
+-- https://github.com/hcchu/dotfiles/blob/master/.xmonad/xmonad.hs
+showVolume = "get-volume.sh"
+changeVolume s = "amixer set Master " ++ s ++ "; get-volume.sh"
+changeBrightness s = "sudo change-brightness.py " ++ s ++ "; show-brightness.sh"
+
+myKeys =
+    [
+        -- http://xmonad.org/xmonad-docs/xmonad-contrib/XMonad-Hooks-ManageDocks.html
+        ((myModMask, xK_b), sendMessage ToggleStruts),
+
+        -- Launch a terminal (changed from return to semicolon)
+        ((myModMask .|. shiftMask, xK_semicolon), spawn myTerminal),
+
+        -- Lock the screen using gnome-screensaver
+        ((winMask, xK_l), spawn "gnome-screensaver-command -l"),
+
+        -- Swap the focused window and the master window
+        -- The default uses return, but semicolon is easier, and
+        -- doesn't conflict with browers =)
+        --((myModMask, xK_semicolon), windows W.swapMaster),
+
+        -- Jump directly to the Full layout
+        ((myModMask, xK_m), sendMessage $ JumpToLayout "Full"),
+        --((myModMask, xK_t), sendMessage $ JumpToLayout "Tall"),
+
+        -- We stole this shortcut above (to emulate DWM's monocle shortcut)
+        -- Lets add a shift modifier.
+        -- Move focus to the master window
+        ((myModMask .|. shiftMask, xK_m), windows W.focusMaster),
+
+        -- Force window back to tiling mode
+        -- ((myModMask .|. shiftMask, xK_t), withFocused $ windows . W.sink),
+
+        ((0, xF86XK_AudioMute), spawn showVolume),
+        ((0, xF86XK_AudioRaiseVolume), spawn $ changeVolume "5%+"),
+        ((0, xF86XK_AudioLowerVolume), spawn $ changeVolume "5%-")
+
+        --((0, xF86XK_MonBrightnessUp), spawn $ changeBrightness "5%+"),
+        --((0, xF86XK_MonBrightnessDown), spawn $ changeBrightness "5%-"),
+        -- https://wiki.archlinux.org/index.php/Lenovo_ThinkPad_X1_Carbon_(Gen_3)
+        -- "Brightness controls are due only in Linux 4.1"
+        --((0, xK_F6), spawn $ changeBrightness "5%+"),
+        --((0, xK_F5), spawn $ changeBrightness "5%-")
+
+        --((controlMask .|. altMask, xK_Left), spawn "xrandr -o right"),
+        --((controlMask .|. altMask, xK_Right), spawn "xrandr -o left"),
+        --((controlMask .|. altMask, xK_Down), spawn "xrandr -o normal"),
+        --((controlMask .|. altMask, xK_Up), spawn "xrandr -o inverted")
+    ]
+
+    ++
+    -- mod-{w,e,r} %! Switch to physical/Xinerama screens 1, 2, or 3
+    -- mod-shift-{w,e,r} %! Move client to screen 1, 2, or 3
+    [((m .|. myModMask, key), screenWorkspace sc >>= flip whenJust (windows . f))
+      | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
+      , (f, m) <- [(W.view, 0), ((\t -> W.view t . W.shift t), shiftMask)]]
+
+main = do 
+    xmproc <- spawnPipe "xmobar"
+    xmonad $ desktopConfig {
+        manageHook = manageDocks <+> manageHook desktopConfig <+> windowPlacement,
+        layoutHook = myLayout,
+        logHook = logHook desktopConfig <+> dynamicLogWithPP xmobarPP {
+            ppOutput = hPutStrLn xmproc,
+            ppTitle = xmobarColor "green" "" . shorten 100
+        } >> updatePointer (Relative 0.95 0.05),
+
+        modMask = myModMask,
+        XMonad.terminal = myTerminal,
+        XMonad.borderWidth = myBorderWidth,
+        startupHook = do
+            setWMName "LG3D"
+            -- spawn "pgrep albert || albert"
+            spawn "pgrep synapse || synapse -s"
+            spawn "pgrep screencloud || screencloud"
+            spawn "pgrep trayer || trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 10 --height 17 --transparent true --alpha 0 --tint 0"
+    } `additionalKeys` myKeys
