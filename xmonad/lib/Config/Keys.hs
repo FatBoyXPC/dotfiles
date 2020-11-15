d4d81c316607c8142ecbb423f5730d19101ec13f
James LaChance <fatboyxpc@gmail.com>
2020-11-15 15:38:23 -0500

    Copied images deserve upload rights, too!
    On a serious note - having the ability to upload images I copied from
    means other than flameshot would be dope. So I made it happen!

--- xmonad/lib/Config/Keys.hs
+++ xmonad/lib/Config/Keys.hs
@@ -67,9 +67,9 @@ myKeys =
         ((controlMask, xF86XK_MonBrightnessUp), spawn "xbacklight -set 100"),
         ((controlMask, xF86XK_MonBrightnessDown), spawn "xbacklight -set `cat ~/.brightness`"),
 
-        ((myModMask, xK_i), spawn "imgur-uploader"),
-        ((myModMask .|. shiftMask, xK_i), spawn "copy-latest-screenshot"),
-        ((controlMask .|. altMask, xK_2), spawn "jscrot --select"),
+        ((myModMask, xK_i), spawn "clipboard-imgur"),
+        ((myModMask .|. shiftMask, xK_i), spawn "copy-latest-upload"),
+        ((controlMask .|. altMask, xK_2), spawn "flameshot gui"),
         ((controlMask .|. altMask, xK_4), spawn "jscrot --video"),
 
         ((0, xF86XK_Display), spawn $ "toggle-display"),
