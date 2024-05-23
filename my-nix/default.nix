4f38a2429fbb5a136e6a3da98eff784f098045ea
James LaChance <fatboyxpc@gmail.com>
2024-05-23 15:01:55 -0400

    Updating chromium through nix!
    I resisted the urge to update via pacman just so I could make
    @jfly happy! That said, I did have to steal some stuff from him:
    
    https://github.com/jfly/snow/blob/0f5d97634227ec72bf6cea7c7f13c681b773b8fd/pattern/desktop.nix#L309-L321

--- my-nix/default.nix
+++ my-nix/default.nix
@@ -1,10 +1,25 @@
 { symlinkJoin, pkgs, shtuff, with-alacritty }:
 
-let wrap-nixgl = pkgs.callPackage ./wrap-nixgl.nix {}; in
+let
+  wrap-nixgl = pkgs.callPackage ./wrap-nixgl.nix {};
+  chromiumAltered = (pkgs.symlinkJoin {
+    name = "chromium";
+    paths = [ pkgs.chromium ];
+    buildInputs = [ pkgs.makeWrapper ];
+      # Adding these as command line flags doesn't seem to work. Perhaps
+      # because we don't have this patch?
+      # https://github.com/archlinux/svntogit-packages/blob/2aa76e8dfdd647d1ca0fe1d8780459660407bad2/chromium/trunk/use-oauth2-client-switches-as-default.patch
+      postBuild = ''
+        wrapProgram $out/bin/chromium \
+          --set GOOGLE_DEFAULT_CLIENT_ID 77185425430.apps.googleusercontent.com \
+          --set GOOGLE_DEFAULT_CLIENT_SECRET OTJgUOQcT7lO7GsGZq2G4IlT
+      '';
+    }); in
 
 symlinkJoin {
   name = "my-nix";
   paths = with pkgs; [
+    chromiumAltered
     mycli
     shtuff
     tldr
